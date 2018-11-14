87584c21aafce8a33a6b66bff804c2b17b623757
Brian L. Troutwine <brian@troutwine.us>
2018-11-14 10:08:36 -0800

    Introduce hashbrown test, failing
    On a tip from @Amanieu I introduced a test of the hashbrown Rust library.
    This test fails on account of we're still kicking out invalid memory
    alignments, looks like. On the plus side this integration test paves
    the way for getting rid of the bin targets and focusing entirely on
    CI.
    
    Signed-off-by: Brian L. Troutwine <brian@troutwine.us>

---
+++ tests/hashbrown.rs
@@ -0,0 +1,30 @@
+extern crate hashbrown;
+extern crate bh_alloc;
+
+#[global_allocator]
+static ALLOC: bh_alloc::BumpAlloc = bh_alloc::BumpAlloc::INIT;
+
+use hashbrown::HashMap;
+
+#[test]
+fn doc_example() {
+    let mut book_reviews = HashMap::new();
+
+    // review some books.
+    book_reviews.insert("Adventures of Huckleberry Finn",    "My favorite book.");
+    book_reviews.insert("Grimms' Fairy Tales",               "Masterpiece.");
+    book_reviews.insert("Pride and Prejudice",               "Very enjoyable.");
+    book_reviews.insert("The Adventures of Sherlock Holmes", "Eye lyked it alot.");
+
+    // check for a specific one.
+    assert!(!book_reviews.contains_key("Les Misérables"));
+
+    // oops, this review has a lot of spelling mistakes, let's delete it.
+    assert!(book_reviews.contains_key("The Adventures of Sherlock Holmes"));
+    book_reviews.remove("The Adventures of Sherlock Holmes");
+    assert!(!book_reviews.contains_key("The Adventures of Sherlock Holmes"));
+
+    // look up the values associated with some keys.
+    assert_eq!(book_reviews.get("Pride and Prejudice"), Some(&"Very enjoyable."));
+    assert_eq!(book_reviews.get("Alice's Adventure in Wonderland"), None);
+}
